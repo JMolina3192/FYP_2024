88c737ddcd0e67fc92dbb86a69d003bb64cce096
Jason <jason.molina@mycit.ie>
2024-04-25 14:36:35 +0000

    testing 16

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # This file is scripts/install_dependencies.sh
 #rm -r /home/ubuntu/scripts/requirements.txt install_dependencies.sh start_app.sh stop_app.sh
-
+apt-get install python3-pip
 pip3 install -r /home/ubuntu/FYP_2024/FYP_2024/scripts/requirements.txt
