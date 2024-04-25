97cecfd5538a48b1df338f121174a49b6c8a6a8a
Jason Molina <jason.molina@mycit.ie>
2024-04-25 23:15:07 +0100

    fixing pipeline.yml

--- scripts/stop_app.sh
+++ scripts/stop_app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SERVICE_FILE=/etc/systemd/system/fyp.service
-if test -f "$SERVICE_FILE"; then
+# SERVICE_FILE=/etc/systemd/system/fyp.service
+# if test -f "$SERVICE_FILE"; then
     sudo systemctl stop fyp.service   # shutdown the service
     # sudo rm "$SERVICE_FILE"
-fi
+# fi
