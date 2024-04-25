cadca14ca24023a408f1b98cf17ea6567dca91d4
Ubuntu <ubuntu@ip-10-0-0-217.eu-west-1.compute.internal>
2024-04-25 14:30:18 +0000

    testing 15

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # This file is scripts/install_dependencies.sh
-rm -r /home/ubuntu/scripts/requirements.txt install_dependencies.sh start_app.sh stop_app.sh
+#rm -r /home/ubuntu/scripts/requirements.txt install_dependencies.sh start_app.sh stop_app.sh
 
 pip3 install -r /home/ubuntu/FYP_2024/FYP_2024/scripts/requirements.txt
