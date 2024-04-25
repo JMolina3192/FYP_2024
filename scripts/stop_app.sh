bfd9460c3e5296ac83f47aef38be639d1140c767
Jason Molina <jason.molina@mycit.ie>
2024-04-25 22:56:57 +0100

    fixing stop_app.sh

--- scripts/stop_app.sh
+++ scripts/stop_app.sh
@@ -3,5 +3,5 @@
 SERVICE_FILE=/etc/systemd/system/fyp.service
 if test -f "$SERVICE_FILE"; then
     sudo systemctl stop fyp.service   # shutdown the service
-    sudo rm "$SERVICE_FILE"
+    # sudo rm "$SERVICE_FILE"
 fi
