4d0b998d4d09aa5dc7258ff4aaa56fe5c1e6d662
Jason Molina <jmolina0392@gmail.com>
2024-04-25 14:48:04 +0100

    testing

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # This file is scripts/install_dependencies.sh
-#rm -r /home/ubuntu/.git/FYP_2024/scripts/requirements.txt install_dependencies.sh start_app.sh stop_app.sh
+rm -r /home/ubuntu/scripts/requirements.txt install_dependencies.sh start_app.sh stop_app.sh
 
-pip3 install -r /home/ubuntu/.git/FYP_2024/scripts/requirements.txt
+pip3 install -r /home/ubuntu/scripts/requirements.txt
